112e8e1b7a6f8c05e5f0dd34f8b75e0e69ccba3e
Robert van Engelen <genivia-inc@users.noreply.github.com>
2023-08-19 16:00:11 -0400

    update

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd corpi
+unzip openssl-3.1.2.zip
+
+tar cvf openssl-3.1.2.tar openssl-3.1.2
+gzip -k openssl-3.1.2.tgz openssl-3.1.2
+
+bzip2 --keep enwik8.bz2 enwik8
+gzip  --keep enwik8.gz  enwik8
+lzip  --keep enwik8.lz4 enwik8
+xz    --keep enwik8.xz  enwik8
+zstd  --keep enwik8.zst enwik8
